e8ee8d5761b37794cbc5cdd7408675b1ccf51ec0
BenAceable <ben@aceable.com>
2022-11-29 14:18:20 -0800

    remove log

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ API_URL = "https://api.nasa.gov/planetary/apod?api_key=your_key_here"
 def download_nasa_image():
     api_response = requests.get(API_URL)
     if api_response.status_code != 200:
-        print(api_response.json())
         raise Exception(api_response.json())
     image_url = api_response.json().get("url")
     file_name = image_url.split("/")[-1]
